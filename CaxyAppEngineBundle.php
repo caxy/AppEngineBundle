d61c897e3a51f82d64917794884daa5e157d3173
Benjamin Doherty <bjd@bangpound.org>
2015-03-03 13:06:10 -0600

    initial commit.

---
+++ CaxyAppEngineBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Caxy\Bundle\AppEngineBundle;
+
+use Caxy\AppEngine\Bridge\Security\Factory;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Bundle\SecurityBundle\DependencyInjection\SecurityExtension;
+
+class CaxyAppEngineBundle extends Bundle
+{
+}
