cc2a942e4d0cd1d52e4ce9c4566e33f392f278a1
Benjamin Doherty <bjd@bangpound.org>
2015-03-07 19:58:37 -0600

    rename services to security.

--- DependencyInjection/CaxyAppEngineExtension.php
+++ DependencyInjection/CaxyAppEngineExtension.php
@@ -23,8 +23,8 @@ class CaxyAppEngineExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
         $loader->load('db.yml');
+        $loader->load('security.yml');
         $loader->load('session.yml');
     }
 }
