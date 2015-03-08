3da5ea66312378bedc05d4a49bc21a3fd9981b30
Benjamin Doherty <bjd@bangpound.org>
2015-03-07 19:54:50 -0600

    extract session services.

--- DependencyInjection/CaxyAppEngineExtension.php
+++ DependencyInjection/CaxyAppEngineExtension.php
@@ -24,5 +24,6 @@ class CaxyAppEngineExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('session.yml');
     }
 }
