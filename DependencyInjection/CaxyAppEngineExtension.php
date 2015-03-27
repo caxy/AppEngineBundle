f9a7e9611d2afd62994ce852c0a8a4aeb5b1d5f9
Benjamin Doherty <bjd@bangpound.org>
2015-03-26 21:40:07 -0500

    mapping of GAE roles to Symfony roles is configurable.

--- DependencyInjection/CaxyAppEngineExtension.php
+++ DependencyInjection/CaxyAppEngineExtension.php
@@ -32,6 +32,9 @@ class CaxyAppEngineExtension extends Extension
         $loader->load('session.yml');
 
         $this->setAppIdentityParameters($container);
+
+        $definition = $container->getDefinition('app_engine.provider.user');
+        $definition->setArguments(array($config['security']['grant_user_role'], $config['security']['grant_admin_role']));
     }
 
     /**
